8d5951b3f180d904e5016b9eb454677a668b8787
nisox <nisoxx@gmail.com>
2017-03-04 13:33:06 +0200

    end

--- New folder/js/app.js
+++ New folder/js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation()
+$(document).foundation();
+$(document).ready(function(){
+  $('.portfolio-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  $('.header-slider').fade({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    accessibility: false,
+    dots: true
+  });
+
+});
