0226da9fe48120d0b01b5b057231c77c64559e9b
Ar4ness <ar4ness@mail.ru>
2017-03-14 17:32:38 +0200

    there left 2 questions

--- New folder/js/app.js
+++ New folder/js/app.js
@@ -7,14 +7,27 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 2000
   });
-  $('.header-slider').fade({
+ /* $('.header-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     accessibility: false,
-    dots: true
+    dots: true;
+  });*/
+
+
+
+  $('.header-slider').slick({
+    accessibility: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    mobileFirst: true
   });
+
 
 });
